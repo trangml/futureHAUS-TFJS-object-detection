49e8fb85f608e220d95ff4629c6dc020b8dc415b
Matthew <mattluutrang@vt.edu>
2021-10-06 00:16:40 -0400

    Pushed initial futureHAUS models and changes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as tf from '@tensorflow/tfjs';
-import {loadGraphModel} from '@tensorflow/tfjs-converter';
+import { loadGraphModel } from '@tensorflow/tfjs-converter';
 import "./styles.css";
 tf.setBackend('webgl');
 
 const threshold = 0.75;
 
 async function load_model() {
-    // It's possible to load the model locally or from a repo
-    // You can choose whatever IP and PORT you want in the "http://127.0.0.1:8080/model.json" just set it before in your https server
-    //const model = await loadGraphModel("http://127.0.0.1:8080/model.json");
-    const model = await loadGraphModel("https://raw.githubusercontent.com/hugozanini/TFJS-object-detection/master/models/kangaroo-detector/model.json");
-    return model;
-  }
+  // It's possible to load the model locally or from a repo
+  // You can choose whatever IP and PORT you want in the "http://127.0.0.1:8080/model.json" just set it before in your https server
+  const model = await loadGraphModel("http://127.0.0.1:8080/model.json");
+  //const model = await loadGraphModel("https://raw.githubusercontent.com/hugozanini/TFJS-object-detection/master/models/kangaroo-detector/model.json");
+  return model;
+}
 
 let classesDir = {
-    1: {
-        name: 'Kangaroo',
-        id: 1,
-    },
-    2: {
-        name: 'Other',
-        id: 2,
-    }
+  1: {
+    name: 'Inverter',
+    id: 1,
+  },
+  2: {
+    name: 'Other',
+    id: 2,
+  }
 }
 
 class App extends React.Component {
@@ -62,29 +62,52 @@ class App extends React.Component {
     }
   }
 
-    detectFrame = (video, model) => {
-        tf.engine().startScope();
-        model.executeAsync(this.process_input(video)).then(predictions => {
-        this.renderPredictions(predictions, video);
-        requestAnimationFrame(() => {
-          this.detectFrame(video, model);
-        });
-        tf.engine().endScope();
+  detectFrame = (video, model) => {
+    tf.engine().startScope();
+    model.executeAsync(this.process_input(video)).then(predictions => {
+      this.renderPredictions(predictions, video);
+      requestAnimationFrame(() => {
+        this.detectFrame(video, model);
       });
+      tf.engine().endScope();
+    });
   };
 
-  process_input(video_frame){
+  process_input(video_frame) {
     const tfimg = tf.browser.fromPixels(video_frame).toInt();
-    const expandedimg = tfimg.transpose([0,1,2]).expandDims();
+    const expandedimg = tfimg.transpose([0, 1, 2]).expandDims();
     return expandedimg;
   };
-
+  buildDetectedObjectsTF1(num_detections, scores, threshold,
+    boxes, classes, classesDir) {
+    const detectionObjects = []
+    var video_frame = document.getElementById('frame');
+    for (var i = 0; i < num_detections[0]; i++) {
+      if (scores[0][i] > threshold) {
+        const bbox = [];
+        const minY = boxes[0][i][0] * video_frame.offsetHeight;
+        const minX = boxes[0][i][1] * video_frame.offsetWidth;
+        const maxY = boxes[0][i][2] * video_frame.offsetHeight;
+        const maxX = boxes[0][i][3] * video_frame.offsetWidth;
+        bbox[0] = minX;
+        bbox[1] = minY;
+        bbox[2] = maxX - minX;
+        bbox[3] = maxY - minY;
+        detectionObjects.push({
+          class: classes[0][i],
+          label: classesDir[classes[0][i]].name,
+          score: scores[0][i].toFixed(4),
+          bbox: bbox
+        })
+      }
+    }
+    return detectionObjects
+  }
   buildDetectedObjects(scores, threshold, boxes, classes, classesDir) {
     const detectionObjects = []
     var video_frame = document.getElementById('frame');
-
     scores[0].forEach((score, i) => {
-      if (score > threshold) {
+      if (score[1] > threshold) {
         const bbox = [];
         const minY = boxes[0][i][0] * video_frame.offsetHeight;
         const minX = boxes[0][i][1] * video_frame.offsetWidth;
@@ -97,7 +120,7 @@ class App extends React.Component {
         detectionObjects.push({
           class: classes[i],
           label: classesDir[classes[i]].name,
-          score: score.toFixed(4),
+          score: score[1].toFixed(4),
           bbox: bbox
         })
       }
@@ -115,11 +138,20 @@ class App extends React.Component {
     ctx.textBaseline = "top";
 
     //Getting predictions
-    const boxes = predictions[4].arraySync();
-    const scores = predictions[5].arraySync();
+    // These indexes work for the tf1 model converted using tfjs_wiz 3.9
+    // const num_detections = predictions[0].dataSync();
+    // const boxes = predictions[5].arraySync();
+    // const scores = predictions[1].arraySync();
+    // const classes = predictions[4].arraySync();
+    // const detections = this.buildDetectedObjectsTF1(num_detections, scores, threshold,
+    // boxes, classes, classesDir);
+
+    // These indexes work for the tf2 model converted using tfjs_wiz 3.9
+    const boxes = predictions[7].arraySync();
+    const scores = predictions[2].arraySync();
     const classes = predictions[6].dataSync();
     const detections = this.buildDetectedObjects(scores, threshold,
-                                    boxes, classes, classesDir);
+      boxes, classes, classesDir);
 
     detections.forEach(item => {
       const x = item['bbox'][0];
@@ -145,17 +177,17 @@ class App extends React.Component {
 
       // Draw the text last to ensure it's on top.
       ctx.fillStyle = "#000000";
-      ctx.fillText(item["label"] + " " + (100*item["score"]).toFixed(2) + "%", x, y);
+      ctx.fillText(item["label"] + " " + (100 * item["score"]).toFixed(2) + "%", x, y);
     });
   };
 
   render() {
     return (
       <div>
-        <h1>Real-Time Object Detection: Kangaroo</h1>
+        <h1>Real-Time Object Detection: FutureHAUS</h1>
         <h3>MobileNetV2</h3>
         <video
-          style={{height: '600px', width: "500px"}}
+          style={{ height: '600px', width: "500px" }}
           className="size"
           autoPlay
           playsInline
@@ -178,3 +210,4 @@ class App extends React.Component {
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
